774f7afa2236bc4c7ce037c350eb761737816c82
Daniel Ryan <dryan@dryan.com>
2014-05-25 14:56:20 -0400

    switched to bower / grunt for build process

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports  =   function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                files: {
+                    'css/smart-grid.css': 'sass/smart-grid.scss',
+                    'css/smart-grid-flexbox.css': 'sass/smart-grid-flexbox.scss',
+                    'css/docs.css': 'sass/docs.scss'
+                }
+            },
+            options: {
+                loadPath: [
+                    'bower_components/bourbon/dist'
+                ],
+                sourcemap: true,
+                style: 'compressed'
+            }
+        },
+        watch: {
+            css: {
+                files: 'sass/*.scss',
+                tasks: ['sass']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['sass']);
+};
